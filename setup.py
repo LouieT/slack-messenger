103f33635761ef0a68668d0e62e24d42db906dc7
Louis Tadman <louis@bienabee.com>
2022-10-28 18:01:40 -0700

    0.1.2 - Updated version in setup.py and deployed

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from setuptools import setup, find_packages
 
-VERSION = '0.1.1'
+VERSION = '0.1.2'
 DESCRIPTION = 'Slack Messanger Client using Incoming Webhooks'
 
 directory = Path(__file__).parent
